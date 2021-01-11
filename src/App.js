a2d3437c2497a62efc521cc1c3d35e34d74a4c7f
Markus Becerra <markusbecerra@gmail.com>
2021-01-11 15:55:58 -0600

    Saved token in client.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import React, { useEffect } from "react";
 import * as $ from "jquery";
 import Player from "./Player";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 
@@ -22,15 +30,9 @@ const scopes = [
 
 
 //extracting hash of the url....
-const hash = window.location.hash.substring(1).split("&").reduce(function(initial,item) {
-  if(item) {
-    let parts = item.split("=");
-    initial[parts[0]] = decodeURIComponent(parts[1]);
-  }
-  return initial;
-}, {});
 
-window.location.hash="";
+
+//window.location.hash="";
 
 
 
@@ -42,7 +44,36 @@ const[is_playing, setIs_playing] = React.useState("Paused");
 const[progress_ms, setProgress_ms] = React.useState(0);
 const[NoData, setNoData] = React.useState(false);
 
+
+const hash = window.location.hash.substring(1).split("&").reduce(function(initial,item) {
+  if(item) {
+    let parts = item.split("=");
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+  }
+  return initial;
+}, {});
+
+//This uses fetch... 
+// const getDatav2 = async (token) => {
+//   const settings = {
+// headers: {'Authorization': 'Bearer ' + token}
+// }
+//   const response = await fetch('https://api.spotify.com/v1/me/player',settings);
+//   const data = await response.json();
+//         if(!data){
+//         setNoData(true);
+//         return;
+//       }
+//       console.log(data);
+//       setItem(data.item);
+//       setIs_playing(data.is_playing);
+//       setProgress_ms(data.progress_ms);
+//       setNoData(false);
+// }
+
+//Original API call function... this uses ajax(a jQuery function) to preform it...
 function getData(token) {
+
   $.ajax({
     url: "https://api.spotify.com/v1/me/player",
     type: "GET",
@@ -54,6 +85,7 @@ function getData(token) {
         setNoData(true);
         return;
       }
+      console.log(data);
       setItem(data.item);
       setIs_playing(data.is_playing);
       setProgress_ms(data.progress_ms);
@@ -63,13 +95,60 @@ function getData(token) {
 }
 
 
+
+//TODO: DO NOT DELETE THE COMMENTED CODE DOWN BELOW. WE SHOULD PROBABLY CHOOSE THE BEST WAY TO USE THE "useEffect" CALLS. SO SHOULD WE 
+// USE STATE VARIABLES OR THE VARIABLES WE DELCARE IN THE "useEffect" FUNCTIONS....
+
+
+// A useEffect is triggered after a component FIRST renders. The useEffect will be called, and then the component will be updated accordingly.
+// See here: https://reactjs.org/docs/hooks-effect.html#:~:text=What%20does%20useEffect%20do%3F,after%20performing%20the%20DOM%20updates.
+//A useEffect within a component will only be called again if a value in its dependency list ( the '[]' at the end of the line) is modifed.
+
+//For example, the first useEffect here has no dependencies, so it will only be called once to retrieve the value of the token in the hash
+//if it exists.
+
+  // useEffect(() => {
+  // const localToken = window.sessionStorage.getItem('token');
+  //   if(localToken) {
+  //   setCurrToken(localToken);
+  // }
+  // else {
+  // let token = hash.access_token
+  // if(token) {
+  //     setCurrToken(token);
+  //     window.sessionStorage.setItem('token',token);
+  // }
+  //  }
+  // }, []);
+
+  
+  //This useEffect will trigger after the component first renders, and then it will trigger whenever 'currtoken' is modified, because 'currtoken' is in its dependencies list.
+  //   useEffect(() => {
+  //     async function fetchData() {
+  //       if(currtoken != null){
+  //         getDatav2(currtoken);
+  //       }
+  //     }
+  //     fetchData();
+  // }, [currtoken]);
+
   useEffect(() => {
   console.log("poop")
-  let token = hash.access_token;
-  if (token) {
-    setCurrToken(token);
+  const localToken = window.sessionStorage.getItem('token')
+  console.log(localToken);
+  if(localToken) {
+    setCurrToken(localToken);
+    getData(localToken);
   }
-  getData(token);
+  else {
+    let token = hash.access_token;
+    console.log(token);
+    if (token) {
+    window.sessionStorage.setItem('token',token);
+    setCurrToken(token);
+    getData(token);
+  }
+  }
 }, []);
 
 
@@ -96,7 +175,7 @@ function getData(token) {
           )}
           {NoData && (
             <p>
-              Are you logged into Spotfiy?
+              Are you logged into Spotify? Make sure you are logged in AND that something is playing.
             </p>
           )}
         </header>

5a3048e5bc33481e6ae7570e9810b5d7b31dbb4b
Haonan Hu <hhn97@ku.edu>
2021-01-12 14:57:42 -0600

    Setup eslint and pretty

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var router = express.Router();
-router.get('/', function(req, res, next) {
-  return res.status(200).json({ message: 'Welcome to Express API template' });
-});
-module.exports = router;
+import express from 'express';
+
+const indexRouter = express.Router();
+
+indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Express API template' }));
+
+export default indexRouter;

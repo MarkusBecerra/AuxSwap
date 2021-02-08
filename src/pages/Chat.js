cd9da944dac86a21cb2a2809a96090872ee90940
Markus Becerra <markusbecerra@gmail.com>
2021-02-08 14:44:05 -0600

    added chat tutorial boilerplate code

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function Chat()
-{
-    return (
-        <h1> HI </h1>
-    )
+import ChatHome from "./ChatHome.js";
+import ChatRoom from "../components/ChatRoom";
+
+function Chat() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/chat" component={ChatHome} />
+        <Route exact path="/:roomId" component={ChatRoom} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default Chat
+export default Chat;
---
+++ src/pages/ChatHome.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./ChatHome.css";
+
+const ChatHome = () => {
+  const [roomName, setRoomName] = React.useState("");
+
+  const handleRoomNameChange = (event) => {
+    setRoomName(event.target.value);
+  };
+
+  return (
+    <div className="home-container">
+      <input
+        type="text"
+        placeholder="Room"
+        value={roomName}
+        onChange={handleRoomNameChange}
+        className="text-input-field"
+      />
+      <Link to={`/${roomName}`} className="enter-room-button">
+        Join room
+      </Link>
+    </div>
+  );
+};
+
+export default ChatHome;

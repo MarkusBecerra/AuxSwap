14042acadc2904a2247eaf3712cec3249c5ad0cc
Markus Becerra <markusbecerra@gmail.com>
2021-02-15 15:00:54 -0600

    Got Spotify Playback to work

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,12 +1,21 @@
-import React from 'react';
-
+import React, { useContext, useEffect } from "react";
+import SpotifyPlayer from 'react-spotify-web-playback';
 import "./ChatRoom.css";
 import useChat from "../hooks/useChat";
+import TokenContext from './TokenContext';
+import * as $ from "jquery";
+import SpotifyTrackMessage from "./SpotifyTrackMessage";
+
+//CREDIT: https://github.com/gilbarbara/react-spotify-web-playback
+
 
 const ChatRoom = (props) => {
+  const regex = /^(spotify:track:|https:\/\/[a-z]+\.spotify\.com\/track\/)([0-9a-z-A-Z]{22})/;
   const { roomId } = props.match.params;
   const { messages, sendMessage } = useChat(roomId);
   const [newMessage, setNewMessage] = React.useState("");
+  const [currSong, setCurrSong] = React.useState("spotify:track:0jBE7Fn78EAvmIs3dCd6GO");
+  const context = useContext(TokenContext);
 
   const handleNewMessageChange = (event) => {
     setNewMessage(event.target.value);
@@ -17,21 +26,44 @@ const ChatRoom = (props) => {
     setNewMessage("");
   };
 
+
+  //Checking if message is spotify track using JS regex.
+  const isMessageSpotifyTrack = (body) =>{
+    if(regex.test(body)){
+      return true;
+    }
+      return false;
+  }
+
+  const setSpotifyURI = (message) => {
+    const array = message.match(regex);
+    const songID = array[2];
+    const res = "spotify:track:".concat(songID);
+    setCurrSong(res);
+  }
+
+
   return (
     <div className="chat-room-container">
       <h1 className="room-name">Room: {roomId}</h1>
-      <div className="messages-container">
-        <ol className="messages-list">
-          {messages.map((message, i) => (
-            <li
-              key={i}
-              className={`message-item ${
-                message.ownedByCurrentUser ? "my-message" : "received-message"
-              }`}
-            >
-              {message.body}
-            </li>
-          ))}
+        <div className="messages-container">
+          <ol className="messages-list">
+          {messages.map((message, i) => {
+            
+            if(isMessageSpotifyTrack(message.body)){
+              return (<li key={i} className={`message-item ${ message.ownedByCurrentUser ? "my-message" : "received-message" }`}>
+                    <div onClick={()=> setSpotifyURI(message.body)}>                    
+                        <SpotifyTrackMessage message={message.body}  />
+                    </div>
+
+              </li>)
+            }
+            else{
+              return (<li key={i} className={`message-item ${ message.ownedByCurrentUser ? "my-message" : "received-message" }`}>
+                          {message.body}
+                     </li>)
+            }
+          })}
         </ol>
       </div>
       <textarea
@@ -43,7 +75,12 @@ const ChatRoom = (props) => {
       <button onClick={handleSendMessage} className="send-message-button">
         Send
       </button>
+      <SpotifyPlayer
+      token={context.currtoken}
+      uris={currSong}
+      />
     </div>
+
   );
 };
 
---
+++ src/components/SpotifyTrackMessage.js
@@ -0,0 +1,41 @@
+import React, { useContext, useEffect, useState } from "react";
+import * as $ from "jquery";
+import './SpotifyTrackMessage.css';
+import TokenContext from './TokenContext';
+
+
+
+const SpotifyTrackMessage = (props) => {
+const context = useContext(TokenContext);  
+ const regex = /^(spotify:track:|https:\/\/[a-z]+\.spotify\.com\/track\/)([0-9a-z-A-Z]{22})/;
+ const [image, setImage] = React.useState("");
+ const getSongImage = (message) => {
+    const array = message.match(regex);
+    const songID = array[2];
+    $.ajax({
+    url: `https://api.spotify.com/v1/tracks/${songID}`,
+    type: "GET",
+    beforeSend: xhr => {
+        xhr.setRequestHeader("Authorization", "Bearer " + context.currtoken);
+    },
+    success: data => {
+      if(!data){
+        setImage("https://i.pinimg.com/originals/d4/e3/60/d4e3604d2811dbe178801f48e6a2ae69.jpg");
+      }
+      console.log(data);
+        setImage(data.album.images[0].url);
+    },
+    error: error => {
+      console.log(error);
+        setImage("https://i.pinimg.com/originals/d4/e3/60/d4e3604d2811dbe178801f48e6a2ae69.jpg");
+
+    }
+  });
+  }
+  useEffect(() => {
+      getSongImage(props.message);
+},[]);
+    return(<img src={image}></img>)
+}
+
+export default SpotifyTrackMessage

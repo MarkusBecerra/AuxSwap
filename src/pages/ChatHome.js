791c042e7dcc340cf70141d9cb3c702b222001f2
Thomas Gardner <thomas.a.gardner22@gmail.com>
2021-03-01 20:27:09 -0600

    add Enter key triggers the join room button in ChatHome.js

--- src/pages/ChatHome.js
+++ src/pages/ChatHome.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import NavBar from '../components/navBar';
 import "./ChatHome.css";
 
 const ChatHome = () => {
   const [roomName, setRoomName] = React.useState("");
+  const history = useHistory();
 
   const handleRoomNameChange = (event) => {
     setRoomName(event.target.value);
   };
+
+const handleClick = () => {
+  history.push(`/chat/${roomName}`);
+}
+
+const handleEnter = e => {    //handle enter function
+  if (e.keyCode == 13) {      //if the user hits enter
+    handleClick()       //call the send message function (basically hit send button)
+  }
+};
 
   return (
     <div>
@@ -18,6 +30,7 @@ const ChatHome = () => {
         type="text"
         placeholder="Room"
         value={roomName}
+        onKeyDown={handleEnter}
         onChange={handleRoomNameChange}
         className="text-input-field"
       />

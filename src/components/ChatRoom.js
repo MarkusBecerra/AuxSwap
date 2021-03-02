bee78e3430466068fc9b1af74d11894c5ad993de
Thomas Gardner <thomas.a.gardner22@gmail.com>
2021-03-01 20:52:51 -0600

    fix all console errors regarding class and className

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -83,7 +83,7 @@ const ChatRoom = (props) => {
 
 
   return (
-  <div class="chat-room-page">
+  <div className="chat-room-page">
     <NavBar/>
    <div className="chat-room-container">
     <h1 className="chat-room-title">Chat Room</h1>

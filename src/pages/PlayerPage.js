ed42646cadcbfb8c0f8ff6247e270721ab70d6c0
Ziwen Wang <ziwangw@gmail.com>
2021-03-07 13:58:17 -0600

    add function for spotify api call add function for handle open player frist time

--- src/pages/PlayerPage.js
+++ src/pages/PlayerPage.js
@@ -1,8 +1,7 @@
-import React, { useContext, useEffect,useCallback } from "react";
+import React, { useContext, useEffect, useCallback } from "react";
 import * as $ from "jquery";
 import Player from "../components/Player";
 import './PlayerPage.css';
-import logo from '../logo.svg';
 import TokenContext from "../components/TokenContext";
 import NavBar from '../components/navBar';
 import SpotifyPlayer from 'react-spotify-web-playback';
@@ -11,54 +10,88 @@ const sp = new SpotifyWebApi()
 
 function PlayerPage() {
 
-const context = useContext(TokenContext);
+  const context = useContext(TokenContext);
 
-const[item, setItem] = React.useState({album: {images: [{url: ""}]}, name: "", artists: [{name: ""}], duration_ms: 0});
-const[is_playing, setIs_playing] = React.useState("Paused");
-const[progress_ms, setProgress_ms] = React.useState(0);
-const[NoData, setNoData] = React.useState(false);
-const[didErrorOccur, setDidErrorOccur] = React.useState(false);
+  const [item, setItem] = React.useState({ album: { images: [{ url: "" }] }, name: "", artists: [{ name: "" }], duration_ms: 0 });
+  const [is_playing, setIs_playing] = React.useState("Paused");
+  const [progress_ms, setProgress_ms] = React.useState(0);
+  const [NoData, setNoData] = React.useState(false);
+  const [didErrorOccur, setDidErrorOccur] = React.useState(false);
+  const [weeklyPlay, setweeklyplay] = React.useState([])
 
-
-//Original API call function... this uses ajax(a jQuery function) to preform it...
-function getData() {
-  $.ajax({
-    url: "https://api.spotify.com/v1/me/player",
-    type: "GET",
-    beforeSend: xhr => {
+  //Original API call function... this uses ajax(a jQuery function) to preform it...
+  function getData() {
+    $.ajax({
+      url: "https://api.spotify.com/v1/me/player",
+      type: "GET",
+      beforeSend: xhr => {
         xhr.setRequestHeader("Authorization", "Bearer " + context.currtoken);
-    },
-    success: data => {
-      if(!data){
+      },
+      success: data => {
+        if (!data) {
+          setNoData(true);
+          return;
+        }
+        setItem(data.item);
+        setIs_playing(data.is_playing);
+        setProgress_ms(data.progress_ms);
+        setNoData(false);
+      },
+      error: error => {
+        console.log(error);
         setNoData(true);
-        return;
+        setDidErrorOccur(true);
+
       }
-      setItem(data.item);
-      setIs_playing(data.is_playing);
-      setProgress_ms(data.progress_ms);
-      setNoData(false);
-    },
-    error: error => {
-      console.log(error);
+    });
+  }
+
+  function getInfor() {
+    //sp.getMyCurrentPlayingTrack()
+    sp.getMyCurrentPlaybackState().then((data) => {
+      if (!data) {
+        Frist_time()
+        setNoData(true);
+        return
+      } else {
+        setItem(data.item);
+        setIs_playing(data.is_playing);
+        setProgress_ms(data.progress_ms);
+        setNoData(false);
+      }
+    }, (error) => {
+      console.log(error)
       setNoData(true);
       setDidErrorOccur(true);
+    });
+    //console.log("called")
+  }
 
-    }
-  });
-}
+  function Frist_time() {
+    sp.getPlaylist('37i9dQZEVXcJZyENOWUFo7').then(
+      (data) => {
+        const res = data.uri
+        const arr = [res];
+        setweeklyplay(arr);
+        //console.log(weeklyPlay);
+        return
+      }, (error) => {
+        console.log(error)
+      }
+    );
+    return
+  }
+
+  //TODO: DO NOT DELETE THE COMMENTED CODE DOWN BELOW. WE SHOULD PROBABLY CHOOSE THE BEST WAY TO USE THE "useEffect" CALLS. SO SHOULD WE 
+  // USE STATE VARIABLES OR THE VARIABLES WE DELCARE IN THE "useEffect" FUNCTIONS....
 
 
+  // A useEffect is triggered after a component FIRST renders. The useEffect will be called, and then the component will be updated accordingly.
+  // See here: https://reactjs.org/docs/hooks-effect.html#:~:text=What%20does%20useEffect%20do%3F,after%20performing%20the%20DOM%20updates.
+  //A useEffect within a component will only be called again if a value in its dependency list ( the '[]' at the end of the line) is modifed.
 
-//TODO: DO NOT DELETE THE COMMENTED CODE DOWN BELOW. WE SHOULD PROBABLY CHOOSE THE BEST WAY TO USE THE "useEffect" CALLS. SO SHOULD WE 
-// USE STATE VARIABLES OR THE VARIABLES WE DELCARE IN THE "useEffect" FUNCTIONS....
-
-
-// A useEffect is triggered after a component FIRST renders. The useEffect will be called, and then the component will be updated accordingly.
-// See here: https://reactjs.org/docs/hooks-effect.html#:~:text=What%20does%20useEffect%20do%3F,after%20performing%20the%20DOM%20updates.
-//A useEffect within a component will only be called again if a value in its dependency list ( the '[]' at the end of the line) is modifed.
-
-//For example, the first useEffect here has no dependencies, so it will only be called once to retrieve the value of the token in the hash
-//if it exists.
+  //For example, the first useEffect here has no dependencies, so it will only be called once to retrieve the value of the token in the hash
+  //if it exists.
 
   // useEffect(() => {
   // const localToken = window.sessionStorage.getItem('token');
@@ -74,7 +107,7 @@ function getData() {
   //  }
   // }, []);
 
-  
+
   //This useEffect will trigger after the component first renders, and then it will trigger whenever 'currtoken' is modified, because 'currtoken' is in its dependencies list.
   //   useEffect(() => {
   //     async function fetchData() {
@@ -84,50 +117,52 @@ function getData() {
   //     }
   //     fetchData();
   // }, [currtoken]);
-  window.onbeforeunload = function () {return false;}
+  window.onbeforeunload = function () { return false; }
   useEffect(() => {
-    const interval =setInterval(()=>{
-      getData();
-    },1000);
-      return () =>{clearInterval(interval)}
-}, []);
+    sp.setAccessToken(context.currtoken)
+    const interval = setInterval(() => {
+      //getData();
+      getInfor();
+    }, 1000);
+    return () => { clearInterval(interval) }
+  }, [context.currtoken]);
 
 
-    return (
-      <div className="App">
-        <NavBar/>
-        <header className="App-header">
-          {!NoData && (
-            <Player
-              item={item}
-              is_playing={is_playing}
-              progress_ms={progress_ms}
-            />
-          )}
-          {NoData && didErrorOccur && (
-            <p>
-                Uh oh! An error occurred when making contact with the API :(
-            </p>
-          )}
-          {NoData && !didErrorOccur && (
-            <p>
-              Are you logged into Spotify? Make sure you are logged in AND that something is playing.
-            </p>
-          )}
-        </header>
-        <div className="spotify_sdk_player">
-          {
-            true ? <SpotifyPlayer
+  return (
+    <div className="App">
+      <NavBar />
+      <header className="App-header">
+        {!NoData && (
+          <Player
+            item={item}
+            is_playing={is_playing}
+            progress_ms={progress_ms}
+          />
+        )}
+        {NoData && didErrorOccur && (
+          <p>
+            Uh oh! An error occurred when making contact with the API :(
+          </p>
+        )}
+        {NoData && !didErrorOccur && (
+          <p>
+            Are you logged into Spotify? Make sure you are logged in AND that something is playing.
+          </p>
+        )}
+      </header>
+      <div className="spotify_sdk_player">
+        {
+          true ? <SpotifyPlayer
             token={context.currtoken}
-            uris={['spotify:artist:6HQYnRM4OzToCYPpVBInuU']}
+            uris={weeklyPlay}
             autoPlay="true"
             showSaveIcon="true"
             name="Auxswap"
-            /> :null
-          }
-        </div>
+          /> : null
+        }
       </div>
-    );
+    </div>
+  );
 
 
 }

2b81135e27375e013eb37a01712f48c3ad157cac
Haonan Hu <hjgyhhn6328@gmail.com>
2021-03-14 15:36:39 -0500

    Backedn CORs update

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 // Import necessary pacakges
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const PORT = 3000;
+const cors = require('cors');
 const db1 = require('./utils/users');
 const db2 = require('./utils/messages');
+
+const app = express();
+const PORT = 4000;
+const corsOptions = {
+    origin: 'http://localhost:3000', //Frontend url
+}
 
 // parsing body parameters
 app.use(bodyParser.json());
@@ -18,25 +23,25 @@ app.get('/', (req, res, next) => {
 });
 
 // Get all users
-app.get('/users', db1.getUser);
+app.get('/users', cors(corsOptions), db1.getUser);
 
 // Get a user by his/her id 
-app.get('/users/:id', db1.getUserById);
+app.get('/users/:id', cors(corsOptions), db1.getUserById);
 
 // add a new user
-app.post('/users', db1.addUser);
+app.post('/users', cors(corsOptions), db1.addUser);
 
 // update an existing user
-app.put('/users/:id', db1.updateUser);
+app.put('/users/:id', cors(corsOptions), db1.updateUser);
 
 // remove an existing user
-app.delete('/users/:id', db1.removeUser);
+app.delete('/users/:id', cors(corsOptions), db1.removeUser);
 
 // Get chat history by session_id
-app.get('/messages/:id', db2.getMessageById);
+app.get('/messages/:id', cors(corsOptions), db2.getMessageById);
 
 // Add a piece of message by session_id
-app.post('/messages/:id', db2.addMessage);
+app.post('/messages/:id', cors(corsOptions), db2.addMessage);
 
 // Start server listening
 app.listen(PORT, () => {

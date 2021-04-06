20f386c384b63fe2fff3f43eb2404cf76bdfc959
Thomas Gardner <thomas.a.gardner22@gmail.com>
2021-04-06 17:07:11 -0500

    add styling to songTab and create songQueue css

--- src/components/Party_components/SongQueue.js
+++ src/components/Party_components/SongQueue.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './SongQueue.css'
 
 export default function SongQueue({song}) {
   
     return (
         <div>
-            <div >
+            <div>
            <img src={song.image}/>
            <div className="song-name">{song.songName}</div>
            <div className="artist-name">{song.artist}</div>
--- src/components/Party_components/SongTab.js
+++ src/components/Party_components/SongTab.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./SongTab.css"
 
 export default function SongTab({song,setsong}) {
     function handleClick(){
@@ -6,10 +7,25 @@ export default function SongTab({song,setsong}) {
         setsong(song)
     }
     return (
-        <div className="song-tab"onClick={handleClick}>
-           <img src={song.image}/>
-           <div className="song-name">{song.songName}</div>
-           <div className="artist-name">{song.artist}</div>
+        // <div className="song-tab"onClick={handleClick}>
+        //    <img src={song.image}/>
+        //    <div className="song-name-party">{song.songName}</div>
+        //    <div className="artist-name-party">{song.artist}</div>
+        // </div>
+
+        <div dclassName="result-container">
+          <ul className="result-list-party">
+              
+                 <li className="song-info-list-item" onClick={handleClick}>
+                      <div>
+                        <img className="search-images" src={song.image} />
+                        <div className="song-name">{song.songName}</div>
+                        <div className="song-artist">{song.artist}</div>
+                      </div>
+                </li>
+                  
+              
+          </ul>
         </div>
     )
 }

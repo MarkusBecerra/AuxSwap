8d7710ed387d06cb24372f41a565d23e6ba4e89d
Markus Becerra <markusbecerra@gmail.com>
2021-04-09 16:34:52 -0500

    fdgsdfgsdfgsdfgsdfgsdfgsdfgsdfg

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const server = require("http").Server(app);
 const cors = require('cors');
 const path = require('path');
-const db1 = require('./utils/users');
-const db2 = require('./utils/messages');
+//const db1 = require('./utils/users');
+//const db2 = require('./utils/messages');
 const db3 = require('./utils/chatroom');
 const app = express();
+const server = require("http").Server(app);
 const io = require("socket.io")(server, {
   cors: {
     origin: '*',
   },
 });
-const {addUser,removeUser,getUsersInRoom,getUser,setPlaylist,getPlaylist,updateplaylist,PeakPlsylist,PopPlaylist} =require('./users.js')
+//const {addUser,removeUser,getUsersInRoom,getUser,setPlaylist,getPlaylist,updateplaylist,PeakPlsylist,PopPlaylist} =require('./users.js')
 const auth = require('./auth');
 app.use(cookieParser());
   app.use(
@@ -34,12 +34,12 @@ app.use(cors(corsOptions));
 app.use('/auth',auth);
 const PORT = process.env.PORT || 4000;
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
-const Join_event = "newJoin"
+// const Join_event = "newJoin"
 
-const Get_room_data = "get_room_data"
-const SS_event="song_send"
-const Get_topList="get_top_list"
-const next_song = "get_next"
+// const Get_room_data = "get_room_data"
+// const SS_event="song_send"
+// const Get_topList="get_top_list"
+// const next_song = "get_next"
 
 // This middleware informs the express application to serve our compiled React files
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
@@ -61,3 +61,25 @@ server.listen(PORT, () => {
   console.log(`App is Listening on port ${PORT}`);
 });
 
+io.on("connection", (socket) => {
+  console.log(`Client ${socket.id} connected`);
+
+  // Join a conversation
+  const { roomId } = socket.handshake.query;
+  if (roomId !=undefined)
+    {socket.join(roomId);}
+
+
+  // Listen for new messages
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+});
+
+
+// Get chat history by session_id
+app.get('/chat/:id', cors(corsOptions), db3.getChatById);
+
+// Add a piece of message by session_id
+app.post('/chat', cors(corsOptions), db3.addChatMessage);

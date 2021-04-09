6c985ffebc5ffa430d97c73204541b6433ead98e
Markus Becerra <markusbecerra@gmail.com>
2021-04-09 17:57:32 -0500

    sadasdad

--- auth.js
+++ auth.js
@@ -91,7 +91,7 @@ auth.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect(`${process.env.REACT_APP_API_URL}/callback#` +
+        res.redirect(`https://auxswaptest.herokuapp.com/callback#` +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token

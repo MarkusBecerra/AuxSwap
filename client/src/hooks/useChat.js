345706b4fddee29380f4a107ee48797b434cfc8b
Markus Becerra <markusbecerra@gmail.com>
2021-04-09 17:32:32 -0500

    dfgsdfgdfgsdf

--- client/src/hooks/useChat.js
+++ client/src/hooks/useChat.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import socketIOClient from "socket.io-client";
 
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
-const SOCKET_SERVER_URL = "http://localhost:4000";
+const SOCKET_SERVER_URL = "https://auxswaptest.herokuapp.com";
 
 const useChat = (roomId) => {
   const [messages, setMessages] = useState([]);
